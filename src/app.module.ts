adf08217d893f2103c74276beeb294f21927a6e3
Satish <satish.mali@scaletech.xyz>
2022-12-14 19:39:34 +0530

    Remove frontend html

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,9 @@ import { AppService } from './app.service';
 import { PollModule } from './modules/poll/poll.module';
 import { EventsModule } from './events/events.module';
 import { DatabaseModule } from './database/database.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'html'),
-    }),
-    PollModule,
-    EventsModule,
-    DatabaseModule,
-  ],
+  imports: [PollModule, EventsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
